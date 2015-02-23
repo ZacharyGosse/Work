01b49da9cb25631a01035505698dcf354f5ee5d1
ssdprojects <ladraythedruid@gmail.com>
2015-02-23 01:42:52 -0500

    new site

--- SecureLogin/SecureLogin/Models/User.cs
+++ SecureLogin/SecureLogin/Models/User.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Data.Entity;
 
 namespace SecureLogin.Models
 {
     public class User
     {
+        public int Id { get; set; }
         public string username { get; set; }
         public string email { get; set; }
         public string password { get; set; }
-        public Image avatar { get; set; }
+        //public Image avatar { get; set; }
 
     }
 
