52607abbf1ca0899f21b04e89b84cd1a2364ce73
ssdprojects <ladraythedruid@gmail.com>
2015-02-23 00:36:49 -0500

    2

---
+++ SecureLogin/SecureLogin/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+using System.Data.Entity;
+
+namespace SecureLogin.Models
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public Image avatar { get; set; }
+
+    }
+
+    public class UserDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
